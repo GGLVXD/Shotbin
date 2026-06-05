e26114331ceae105922f3c0cea6a8af5e3fde55d
GGLVXD <admin@gglvxd.net>
2026-06-05 09:21:29 +0300

    remove

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,15 +2,12 @@
  
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        if (config('app.env') === 'production') {
-            URL::forceSchema('https');
-        }
     }
 }
